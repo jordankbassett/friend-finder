57bca7551b7132e38624a4f48dc6de621d5348c8
Jordan <jordankbassett@gmail.com>
2020-09-26 13:52:02 -0500

    m

---
+++ friendfinder/data/friends.js
@@ -0,0 +1,28 @@
+var friends = [
+    {
+      name: "Thing 1",
+      photo:
+        "",
+      scores: [1, 2, 3, 5, 2, ]
+    },
+    {
+      name: "Thing 2",
+      photo:
+        "",
+      scores: [2, 2, 3, 2, 2,]
+    },
+    {
+      name: "Thing 3",
+      photo:
+        "",
+      scores: [1, 4, 5, 1, 1, ]
+    },
+    {
+      name: "Thing 4",
+      photo:
+        "",
+      scores: [2, 4, 5, 1, 1, ]
+    }
+];
+
+module.exports = friends;
